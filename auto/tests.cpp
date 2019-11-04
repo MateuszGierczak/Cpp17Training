373ed8a67804a623482b2f95766a2062566015e4
Mateusz Gierczak <mateusz.gierczak33@gmail.com>
2019-11-04 14:57:00 +0100

    Implement examples for ctad

--- auto/tests.cpp
+++ auto/tests.cpp
@@ -3,13 +3,8 @@
 
 #include <iostream>
 
-void function() {}
-
-template<typename T, std::size_t N>
-constexpr std::size_t array_size(const T(&)[N])
-{
-   return N;
-}
+void function()
+{}
 
 TEST_CASE("auto")
 {
@@ -28,21 +23,20 @@ TEST_CASE("auto")
         auto&& uref3 = 27;
         auto&& uref4 = std::move(uref3);
 
-        //reference collapsing
-        REQUIRE(std::is_same<decltype(uref1), int&>::value); //auto&& &
-        REQUIRE(std::is_same<decltype(uref2), const int&>::value); //const auto&& &
-        REQUIRE(std::is_same<decltype(uref3), int&&>::value); //auto&&
-        REQUIRE(std::is_same<decltype(uref4), int&&>::value); //auto&& && -> auto&&
+        // reference collapsing
+        REQUIRE(std::is_same<decltype(uref1), int&>::value); // auto&& &
+        REQUIRE(std::is_same<decltype(uref2), const int&>::value); // const auto&& &
+        REQUIRE(std::is_same<decltype(uref3), int&&>::value); // auto&&
+        REQUIRE(std::is_same<decltype(uref4), int&&>::value); // auto&& && -> auto&&
     }
 
     SECTION("auto with c-style array")
     {
-        const int arr[] = {1, 2, 3, 4};
+        const int arr[] = { 1, 2, 3, 4 };
 
         auto arr1 = arr;
         auto& arr2 = arr;
 
-        static_assert(array_size(arr) == 4, "");
         REQUIRE(std::is_same<decltype(arr1), const int*>::value);
         REQUIRE(std::is_same<decltype(arr2), const int(&)[4]>::value);
     }
@@ -52,21 +46,20 @@ TEST_CASE("auto")
         auto fun1 = function;
         auto& fun2 = function;
 
-        REQUIRE(std::is_same<decltype(fun1), void(*)()>::value);
-        REQUIRE(std::is_same<decltype(fun2), void(&)()>::value);
-
+        REQUIRE(std::is_same<decltype(fun1), void (*)()>::value);
+        REQUIRE(std::is_same<decltype(fun2), void (&)()>::value);
     }
 
     SECTION("auto vs. curly braces")
     {
         auto x1 = 27;
         auto x2(27);
-        auto x3 = {27};
-        auto x4 {27};
+        auto x3 = { 27 };
+        auto x4{ 27 };
 
-        //INTERESTING FACT
-        //int x5 {4.5}; //narrowing conversion takes place here but compiler reports error
-        int x6(4.5); //narrowing conversion takes place here but compiler doesn't report it
+        // INTERESTING FACT
+        // int x5 {4.5}; //narrowing conversion takes place here but compiler reports error
+        int x6(4.5); // narrowing conversion takes place here but compiler doesn't report it
 
         REQUIRE(std::is_same<int, decltype(x1)>::value);
         REQUIRE(std::is_same<int, decltype(x2)>::value);
@@ -87,14 +80,16 @@ void noexcept_function() noexcept
 
 TEST_CASE("noexpect as a part of function signature")
 {
-    REQUIRE_FALSE(std::is_same<decltype(non_noexcept_function), decltype(noexcept_function)>::value); //Before cpp17 type of these two functions was same
+    REQUIRE_FALSE(std::is_same<decltype(non_noexcept_function), decltype(noexcept_function)>::
+                      value); // Before cpp17 type of these two functions was same
 
-    //INTERESTING FACT
-    void (*ptr_fun)() = noexcept_function; //non noexcept function pointer binds noexcept function
+    // INTERESTING FACT
+    void (*ptr_fun)() = noexcept_function; // non noexcept function pointer binds noexcept function
     ptr_fun();
 
     ptr_fun = non_noexcept_function;
     ptr_fun();
 
-    //void (*ptr_fun1)() noexcept = non_noexcept_function; //it doesn't work because noexcept function pointer can't bind non noexcept function
+    // void (*ptr_fun1)() noexcept = non_noexcept_function; //it doesn't work because noexcept
+    // function pointer can't bind non noexcept function
 }
--- class-template-arg-deduction/tests.cpp
+++ class-template-arg-deduction/tests.cpp
@@ -1,8 +1,207 @@
 #include "catch.hpp"
 #include "range_printer.hpp"
 
-#include <iostream>
+#include <boost/type_index.hpp>
 
-TEST_CASE("Test")
+#include <iostream>
+#include <map>
+#include <string>
+#include <tuple>
+
+using namespace boost::typeindex;
+
+template <typename T>
+void value_arg_function(T x)
 {
+    std::cout << __FUNCTION__ << ": " << type_id_with_cvr<decltype(x)>().pretty_name() << "\n";
+}
+
+template <typename T>
+void ref_arg_function(T& x)
+{
+    std::cout << __FUNCTION__ << ": " << type_id_with_cvr<decltype(x)>().pretty_name() << "\n";
+}
+
+template <typename T, std::size_t N>
+constexpr std::size_t array_size(const T (&)[N])
+{
+    return N;
+}
+
+template <typename T>
+void uref_arg_function(T&& x)
+{
+    std::cout << __FUNCTION__ << ": " << type_id_with_cvr<decltype(x)>().pretty_name() << "\n";
+}
+
+TEST_CASE("Template argument deduction")
+{
+    int x = 27;
+    const int const_x = 27;
+    int& ref_x = x;
+    const int& const_ref_x = const_x;
+
+    constexpr std::size_t ARRAY_SIZE = 10;
+    int arr[ARRAY_SIZE] = {};
+
+    SECTION("function argument as value")
+    {
+        value_arg_function(x); // T = int
+        value_arg_function(const_x); // T = int, constness is ignored
+        value_arg_function(ref_x); // T = int, referencensess is also ignored
+        value_arg_function(const_ref_x); // T = int, constness and referencness is ignored
+        value_arg_function(arr); // T = int*, c-style array is pointer to first element
+    }
+
+    SECTION("function argument as ref")
+    {
+        ref_arg_function(x);
+        ref_arg_function(const_x);
+        ref_arg_function(ref_x);
+        ref_arg_function(const_ref_x);
+        ref_arg_function(arr);
+
+        static_assert(array_size(arr) == ARRAY_SIZE);
+    }
+
+    SECTION("function argument as universal ref")
+    {
+        uref_arg_function(x); // T = int&, reference collapsing T&& & = T&
+
+        int&& rref_x = 27;
+        uref_arg_function(std::move(rref_x)); // T = int&&, reference collapsing T&& && = T&&
+
+        uref_arg_function(27); // T = int&&, same rule as above
+    }
+}
+
+namespace training
+{
+    template <typename T, typename U>
+    struct pair
+    {
+        T first_{};
+        U second_{};
+
+        pair(const T& first, const T& second) : first_{ first }, second_{ second }
+        {}
+
+        pair(T&& first, U&& second) : first_{ std::move(first) }, second_{ std::move(second) }
+        {}
+    };
+}
+
+TEST_CASE("Class template argument deduction - how it works")
+{
+    using namespace std::string_literals;
+
+    training::pair p1{ "THX"s, 694 };
+    REQUIRE(std::is_same_v<decltype(p1), training::pair<std::string, int>>);
+
+    // HOW IT WORKS
+    // REFERENCE -> Class Template Argument Deduction in C++17 - Timur Doumler [ACCU 2018]
+    // 1. Check if class is template class
+    // 2. Treat constructors as functions
+    // 3. Submit template type deduction and overload resolution to choose function
+    // 4. Deduce types for class
+    // 5. Instanstiate it
+}
+
+TEST_CASE("Class template argument deduction")
+{
+    SECTION("with std::pair")
+    {
+        // In cpp98
+        std::pair<std::string, int> p1{ "THX", 1011 };
+
+        // In cpp11
+        auto p2 = std::make_pair("THX", 1011); // p2 = std::pair<const char*, int>
+
+        // In cpp14
+        using namespace std::string_literals;
+
+        auto p3 = std::make_pair("THX"s, 1011);
+
+        // In cpp17
+        std::pair p4{ "THX"s, 1011 };
+        REQUIRE(std::is_same_v<decltype(p4), std::pair<std::string, int>>);
+    }
+
+    SECTION("with std::tuple")
+    {
+        using namespace std::string_literals;
+
+        std::tuple<std::string, int, double> t1{ "THX", 1011, 10.5 };
+        std::tuple t2{ "THX"s, 1011, 10.5 };
+
+        REQUIRE(std::is_same_v<decltype(t1), decltype(t2)>);
+    }
+
+    SECTION("class vs template type deduction - rule all or nothing")
+    {
+        // std::tuple<std::string> t1{ "THX", 1011, 10.5 }; - ERROR: partially specified class
+        // template argument
+
+        auto t2 = std::make_tuple<std::string>("THX", 1011, 10.5);
+    }
+
+    SECTION("with std::vector")
+    {
+        std::vector<int> v1{ 3 };
+        std::cout << "v1 = " << utils::print_range(v1) << "\n";
+
+        std::vector<int> v2(3);
+        std::cout << "v2 = " << utils::print_range(v2) << "\n";
+
+        std::vector v3{ 3 };
+        // std::vector v4(3); - class template argument deduction dosnt work here
+
+        std::vector range{ 1, 2, 3, 4, 5, 10 };
+        std::vector copy_range(std::begin(range), std::end(range)); // but how it works ?
+
+        std::cout << type_id_with_cvr<decltype(copy_range)>().pretty_name() << "\n";
+    }
+
+    SECTION("with std::map")
+    {
+        using namespace std::string_literals;
+
+        // std::map m1{ { "THX"s, 1011 }, { "THZ"s, 1012 } }; - it doesn't work :(
+        std::map<std::string, int> m2{ { "THX"s, 1011 }, { "THZ"s, 1012 } };
+    }
+}
+
+namespace training
+{
+    template <typename T>
+    struct vector
+    {
+        template <typename Iterator>
+        vector(Iterator begin, Iterator end)
+        {
+            /*...*/
+        }
+    };
+
+    template <typename Iterator>
+    vector(Iterator, Iterator)->vector<typename std::iterator_traits<Iterator>::value_type>;
+}
+
+TEST_CASE("Deduction guide")
+{
+    std::vector range{ 1, 2, 3, 4, 10 };
+
+    training::vector copy_range1(std::begin(range), std::end(range));
+    std::cout << type_id_with_cvr<decltype(copy_range1)>().pretty_name() << "\n";
+
+    REQUIRE(std::is_same_v<decltype(copy_range1), training::vector<int>>);
+
+    SECTION("list-initialisation has priority")
+    {
+        std::vector v1(std::begin(range), std::end(range));
+        REQUIRE(std::is_same_v<decltype(v1), std::vector<int>>);
+
+        std::vector v2{ std::begin(range), std::end(range) };
+        REQUIRE_FALSE(std::is_same_v<decltype(v2), std::vector<int>>);
+    }
 }
