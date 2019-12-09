975a3e28330f30c28733112cb5edc182e4be2237
Mateusz Gierczak <mateusz.gierczak33@gmail.com>
2019-12-09 09:29:44 +0100

    Add exercise for CTAD

---
+++ _exercises/CTAD/tests.cpp
@@ -0,0 +1,22 @@
+#include "catch.hpp"
+#include "range_printer.hpp"
+
+#include <iostream>
+
+template<typename T, std::size_t N>
+struct array
+{
+    T data[N];
+};
+
+template<typename T, typename... Ts>
+array(T, Ts...) -> array<std::enable_if_t<(std::is_same_v<T, Ts> && ...), T>,
+                         1 + sizeof...(Ts)>;
+
+TEST_CASE("Write deduction guide for array type")
+{
+    array a1{10, 20, 30, 40};
+    REQUIRE(std::size(a1.data) == 4);
+
+    array a2 {10.0, 30}; // it should not compile
+}
